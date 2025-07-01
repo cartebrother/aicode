cf7d326e6b13704d8e97780191173fa1d6d4d439
李福春 <505847426@qq.com>
2025-07-01 23:14:39 +0800

    python基础学习

---
+++ p3/day2/bl.py
@@ -0,0 +1,14 @@
+print(1 + 2)
+print(1 - 2)
+print(1 * 2)
+print(1 / 2)
+
+print("================")
+x = 1
+y = 2
+z = x + y
+print(z)
+print(x - y)
+print(x * y)
+print(x / y)
+print(z * 100)
---
+++ p3/day2/mark.py
@@ -0,0 +1,13 @@
+#导入模块 单行注释
+import datetime
+
+#输出当前日期，并指定格式
+print(datetime.datetime.now().strftime("%Y-%m-%d %X"))
+
+"""
+多行注释
+打印一个10以内的随机数
+"""
+
+import random
+print(random.randint(1,10))
